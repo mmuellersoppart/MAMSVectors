82348963fa7a5f75ff87d871d24fb0002e6db087
mmuellersoppart <mmuellersoppart@gmail.com>
2022-04-07 06:33:01 -0500

    make operators public

--- Sources/MAMS_Vector/Interactions.swift
+++ Sources/MAMS_Vector/Interactions.swift
@@ -1,12 +1,12 @@
-func +(left: Point2D, right: Vector2D) -> Point2D {
+public func +(left: Point2D, right: Vector2D) -> Point2D {
     Point2D(x: left.x + right.x, y: left.y + right.y)
 }
 
-func +(left: Vector2D, right: Point2D) -> Point2D {
+public func +(left: Vector2D, right: Point2D) -> Point2D {
     Point2D(x: left.x + right.x, y: left.y + right.y)
 }
 
-func *(left: Double, right: PositionalVector2D) -> PositionalVector2D {
+public func *(left: Double, right: PositionalVector2D) -> PositionalVector2D {
     PositionalVector2D(originX: right.origin.x, originY: right.origin.y, vectorX: right.vector.x * left, vectorY: right.vector.y * left)
 }
 
