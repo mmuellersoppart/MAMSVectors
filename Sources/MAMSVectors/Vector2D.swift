b2b07a23f502e5fb39bc7afb45a82613e48f3114
mmuellersoppart <mmuellersoppart@gmail.com>
2022-04-18 07:05:12 -0500

    add path and draw to vector2D

--- Sources/MAMSVectors/Vector2D.swift
+++ Sources/MAMSVectors/Vector2D.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import CoreGraphics
+import SwiftUI
 
 /// A 2-dimensional vector (CGVector)
 public struct Vector2D {
@@ -52,7 +53,22 @@ extension Vector2D {
     }
 }
 
-// Connection with SwiftUI
+
+// Connection to Core Graphics
+@available(iOS 15.0, *)
+@available(macOS 15.0, *)
+extension Point2D {
+    public func asPath() -> Path {
+        Path { path in
+            path.addLine(to: asCGPoint)
+        }
+    }
+
+    public func draw(context: inout GraphicsContext) {
+        let vector2DPath = asPath()
+        context.stroke(vector2DPath, with: .color(.red))
+    }
+}
 
 extension Vector2D {
 
